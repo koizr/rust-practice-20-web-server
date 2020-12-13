99d6a1c5c03145b6cc4320f5c47cd586e98dc53e
koizr <koizumi.ryo.dev@gmail.com>
2020-12-13 23:33:49 +0900

    wait responses for processing requests if web server shut down

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 trait FnBox {
@@ -19,6 +19,11 @@ impl<F: FnOnce()> FnBox for F {
 }
 
 type Job = Box<dyn FnBox + Send + 'static>;
+
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
 
 impl ThreadPool {
     /// ThreadPool を生成する
@@ -47,26 +52,59 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static,
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        // 仕事がない worker からメッセージを受信するので、 1 つのループでメッセージの送信と join を行ってしまうと
+        // 終了することがない worker の終了を待ち続けて停止しなくなるパターンがある。
+        // なので terminate を投げるループと join するループを分ける必要がある。
+        // 参照: https://doc.rust-jp.rs/book-ja/ch20-03-graceful-shutdown-and-cleanup.html
+        println!("Sending terminate message to all workers.");
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutting down all workers.");
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         // while let Ok(job) = receiver.lock().unwrap().recv().unwrap() にすることもできなくはないけど、
         // while let にするとスコープが変わって job のライフライムが伸びるので、 Worker が次の Job を受けることができなくなってしまうらしい。
         // 詳細: https://doc.rust-jp.rs/book-ja/ch20-02-multithreaded.html の一番下。
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {} got a job; executing.", id);
-            job.call_box();
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job.call_box();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
     }
 }
